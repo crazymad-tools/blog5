93b61a8e99fe1c407374666517ee8f648492a158
crazy_mad <crazymad0601@gmail.com>
2019-12-25 12:01:23 +0800

    ssr初始化

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/51014496814370826.png" />
+    </div>
+  );
+}
+
+export default App;
